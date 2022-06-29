7253fcf6c7ab570cc13bd5223ec8ce9f43e9b7f9
Arion Dantas <ariondantas02&@gmail.com>
2022-06-29 11:04:57 -0300

    Descobrir seu IMC

--- aula1.py
+++ aula1.py
@@ -1,7 +1,20 @@
-print('Hello Word')
+#algoritimo para descobrir o seu IMC (indice de massa corporal)
 
+peso = float(input('qual o seu peso: '))
+altura = float(input('qual a sua altura: '))
 
-print('arion is beautiful')
-
-
-print('teste de commit')
+descobrirImc = peso / (altura ** 2)
+print('O seu IMC é {:.1f}' .format(descobrirImc))
+if descobrirImc < 18.5:
+    print('você está abaixo do peso')
+elif descobrirImc >= 18.5 and descobrirImc < 25 :
+    print('você está saudável')
+elif descobrirImc >= 25 and descobrirImc < 30:
+    print('Peso em excesso')
+elif descobrirImc >= 30 and descobrirImc < 35 :
+    print('Obesidade grau I')
+elif descobrirImc >= 35 and descobrirImc < 40 :
+    print('Obesidade grau II')
+elif descobrirImc >= 40:
+    print('Obesidade grau III')
+    
